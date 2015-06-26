6e1810546dce3fd0440525708016a118b610d958
Gautam <gautam.sisodia@gmail.com>
2015-06-26 09:42:09 -0400

    updates

--- website/app/views.py
+++ website/app/views.py
@@ -5,14 +5,13 @@ import time as tm
 import pickle
 
 
-
 @app.route('/')
 def cities_output():
   #pull 'ID' from input field and store it
   #thread_url = request.args.get('thread')
   comment = request.args.get('comment')
 
-  model_file = open('text_mnb_first_dump', 'r')
+  model_file = open('mnb_dump_2', 'r')
   model = pickle.load(model_file)  
 
   suggestions = []
@@ -28,4 +27,5 @@ def cities_output():
     score = round(100*score)
     
   return render_template("index.html", comment = comment, score = score)
+  
   
